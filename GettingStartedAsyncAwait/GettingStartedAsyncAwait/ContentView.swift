a9fda320815394ece77a77a720f420275a63fed9
applebuddy <eagermin92@gmail.com>
2022-10-19 03:00:08 +0900

    Study lecture 12. Implementing getDate Async(async throws) Function

--- GettingStartedAsyncAwait/GettingStartedAsyncAwait/ContentView.swift
+++ GettingStartedAsyncAwait/GettingStartedAsyncAwait/ContentView.swift
@@ -18,6 +18,17 @@ struct CurrentDate: Decodable, Identifiable {
 }
 
 struct ContentView: View {
+  /// 메서드 안에서 try await을 사용하는 경우, 해당 메서드 반환부 앞에 async throws를 명시해 주어야 한다.
+  private func getDate() async throws -> CurrentDate? {
+    guard let url = URL(string: "https://ember-sparkly-rule.glitch.me/current-date") else {
+      fatalError("URL is incorrect!")
+    }
+    
+    // async function은 사용 시 await를 앞에 붙혀주어야 한다.
+    // (async throws -> try await을 사용해야함)
+    let (data, _) = try await URLSession.shared.data(from: url)
+    return try? JSONDecoder().decode(CurrentDate.self, from: data)
+  }
   
   var body: some View {
     NavigationView {
