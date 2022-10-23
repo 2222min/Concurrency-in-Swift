fd391dbc16e8e86af386fa462c0d94c78d18c272
applebuddy <eagermin92@gmail.com>
2022-10-23 19:19:05 +0900

    Add project for Section 9: Project Time - Random Images and Random Quotes

---
+++ RandomQuoteAndImages/RandomQuoteAndImages/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RandomQuoteAndImages
+//
+//  Created by MinKyeongTae on 2022/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RandomQuoteAndImages/RandomQuoteAndImages/RandomQuoteAndImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomQuoteAndImagesApp.swift
+//  RandomQuoteAndImages
+//
+//  Created by MinKyeongTae on 2022/10/23.
+//
+
+import SwiftUI
+
+@main
+struct RandomQuoteAndImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
