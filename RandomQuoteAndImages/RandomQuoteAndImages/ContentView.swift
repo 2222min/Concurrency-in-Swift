d76c9b375172082b05f1add61dbb1caf2d7ff9fb
applebuddy <eagermin92@gmail.com>
2022-10-23 22:15:10 +0900

    Study lecture 48. Displaying Random Images and Quotes on Screen

--- RandomQuoteAndImages/RandomQuoteAndImages/ContentView.swift
+++ RandomQuoteAndImages/RandomQuoteAndImages/ContentView.swift
@@ -8,14 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  
+  @StateObject private var randomImageListViewModel = RandomImageListViewModel()
+
+  var body: some View {
+    List(randomImageListViewModel.randomImages) { randomImage in
+      HStack {
+        randomImage.image.map {
+          Image(uiImage: $0)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+        }
+        Text(randomImage.quote)
+      }
+    }.task {
+      // onAppear일때 async 동작을 취할 수 있다. (await 사용 가능)
+      await randomImageListViewModel.getRandomImages(ids: Array(100...120))
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
